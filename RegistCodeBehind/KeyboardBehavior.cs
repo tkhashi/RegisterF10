c541c8b713706caa2fa058520280905aeb061478
KazuhiroTakahashi <k_takahashi@just-ltd.co.jp>
2022-04-03 03:08:15 +0900

    F9ショートカット実装

---
+++ RegistCodeBehind/KeyboardBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace RegistCodeBehind
+{
+    public class KeyboardBehavior : Behavior<FrameworkElement>
+    {
+        public static readonly DependencyProperty KeyTypeProperty =
+            DependencyProperty.Register(
+                nameof(KeyType),
+                typeof(Key),
+                typeof(KeyboardBehavior),
+                new FrameworkPropertyMetadata());
+
+        public static readonly DependencyProperty KeyDownProperty =
+            DependencyProperty.Register(
+                nameof(KeyDown),
+                typeof(ICommand),
+                typeof(KeyboardBehavior),
+                new FrameworkPropertyMetadata());
+
+        public Key KeyType
+        {
+            get => (Key) GetValue(KeyTypeProperty);
+            set => SetValue(KeyTypeProperty, value);
+        }
+
+        public ICommand KeyDown
+        {
+            get => (ICommand) GetValue(KeyDownProperty);
+            set => SetValue(KeyDownProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.KeyDown += OnKeyDown;
+            var window = Application.Current.MainWindow;
+            if (window == null) return;
+
+            window.KeyDown += OnKeyDown;
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            var isF9 = e.Key == KeyType;
+            var canExecuteF9Command = KeyDown.CanExecute(null);
+
+            if (isF9 && canExecuteF9Command)
+            {
+                KeyDown.Execute(null);
+            }
+        }
+    }
+}
---
+++ RegistCodeBehind/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System.Reactive.Disposables;
+
+namespace RegistCodeBehind
+{
+    public class MainWindowViewModel
+    {
+
+        public ReactivePropertySlim<string> F9Text { get; } = new ReactivePropertySlim<string>("何も押されてないよ");
+        public ReactivePropertySlim<string> F10Text { get; } = new ReactivePropertySlim<string>("何も押されてないよ");
+        public ReactiveCommand F9Command { get; } = new ReactiveCommand();
+        public ReactiveCommand F10Command { get; } = new ReactiveCommand();
+        private CompositeDisposable _disposable = new CompositeDisposable();
+
+       public MainWindowViewModel()
+       {
+           F9Command.Subscribe(OnF9Down)
+               .AddTo(_disposable);
+       }
+
+       private void OnF9Down()
+       {
+           F9Text.Value = "F9が押されたよ";
+       }
+       private void OnF10Down()
+       {
+           F10Text.Value = "F10が押されたよ";
+       }
+    }
+}
